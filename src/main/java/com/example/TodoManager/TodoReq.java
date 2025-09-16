423db376fd55842e5242454da67013834f060144
LANA.BI <LANA.BI@OOCL.COM>
2025-09-16 15:33:40 +0800

    feat:add todoreq

---
+++ src/main/java/com/example/TodoManager/TodoReq.java
@@ -0,0 +1,31 @@
+package com.example.TodoManager;
+
+public class TodoReq {
+    private long id;
+    private String text;
+    private boolean done;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
