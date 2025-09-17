cafd8ae4e6f8da5e87da20fa5667e4235791dea3
LANA.BI <LANA.BI@OOCL.COM>
2025-09-17 09:32:25 +0800

    refactor： use lombok getter and setter in todoreq

--- src/main/java/com/example/TodoManager/TodoReq.java
+++ src/main/java/com/example/TodoManager/TodoReq.java
@@ -1,22 +1,12 @@
 package com.example.TodoManager;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class TodoReq {
     private String text;
     private boolean done;
 
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public boolean isDone() {
-        return done;
-    }
-
-    public void setDone(boolean done) {
-        this.done = done;
-    }
 }
